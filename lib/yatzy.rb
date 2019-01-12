4b586f81a4e15e30a49002e99c03d098b55019a3
Krzysztof Kulewski <krzysztofkulewski@gmail.com>
2019-01-12 14:10:15 +0100

    Rename set_tallies to get_tallies + use in yatzy scores

--- lib/yatzy.rb
+++ lib/yatzy.rb
@@ -4,11 +4,8 @@ class Yatzy
     die.sum
   end
 
-  def self.yatzy(dice)
-    tallies = [0] * (dice.length + 1)
-    dice.each do |i|
-      tallies[i - 1] += 1
-    end
+  def self.yatzy(die)
+    tallies = get_tallies(die)
     (0..tallies.size).each do |i|
       return 50 if tallies[i] == 5
     end
@@ -24,7 +21,7 @@ class Yatzy
   end
 
   def self.score_pair(die)
-    tallies = set_tallies(die)
+    tallies = get_tallies(die)
     (0...tallies.size).reverse_each do |i|
       return (i + 1) * 2 if tallies[i] >= 2
     end
@@ -32,7 +29,7 @@ class Yatzy
   end
 
   def self.two_pair(die)
-    tallies = set_tallies(die)
+    tallies = get_tallies(die)
     n = 0; score = 0
     tallies.each_with_index do |i, ind|
       if i >= 2
@@ -44,7 +41,7 @@ class Yatzy
     0
   end
 
-  def self.set_tallies(die)
+  def self.get_tallies(die)
     tallies = [0] * 6
     (0..4).each do |i|
       tallies[die[i] - 1] += 1
@@ -53,7 +50,7 @@ class Yatzy
   end
 
   def self.number_of_a_kind(die, number)
-    tallies = set_tallies(die)
+    tallies = get_tallies(die)
     (0..tallies.size).each do |i|
       return (i + 1) * number if tallies[i] >= number
     end
@@ -61,7 +58,7 @@ class Yatzy
   end
 
   def self.small_straight(die)
-    tallies = set_tallies(die)
+    tallies = get_tallies(die)
     all_ones = true
     tallies.each do |i|
       all_ones = false if tallies[i] != 1
@@ -70,7 +67,7 @@ class Yatzy
   end
 
   def self.large_straight(die)
-    tallies = set_tallies(die)
+    tallies = get_tallies(die)
     all_ones = true
     tallies.each do |i|
       all_ones = false if tallies[i + 1] != 1
@@ -79,7 +76,7 @@ class Yatzy
   end
 
   def self.full_house(die)
-    tallies = set_tallies(die)
+    tallies = get_tallies(die)
     find_number(tallies, 2) * 2 + find_number(tallies, 3) * 3
   end
 

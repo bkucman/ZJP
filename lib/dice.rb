c30c198cdc7094f3bed8461e7c049875f20a1ddc
Krzysztof Kulewski <krzysztofkulewski@gmail.com>
2019-01-12 12:56:31 +0100

    Use consistent indent

--- lib/dice.rb
+++ lib/dice.rb
@@ -1,9 +1,9 @@
 class Dice
-    def initialize(d1, d2, d3, d4, d5)
-      @dice = [d1, d2, d3, d4, d5]
-    end
+  def initialize(d1, d2, d3, d4, d5)
+    @dice = [d1, d2, d3, d4, d5]
+  end
 
-    def return_dice()
-        @dice
-    end
+  def return_dice()
+    @dice
+  end
 end
